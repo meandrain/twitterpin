14487c439391a383300aa9ae2a91518651521a7b
Adrian Manolache <adrian.manolachecg@gmail.com>
2014-03-14 19:03:51 +0200

    * initial empty project

---
+++ twitterpin/twitterpin/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  twitterpin
+//
+//  Created by Adrian Manolache on 14/03/14.
+//  Copyright (c) 2014 Adrian Manolache. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
